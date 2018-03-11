32e15af1a9e64d30318eb91e5b7f67d639332679
Clément de Louvencourt <clement.delouvencourt@gmail.com>
2018-03-11 13:43:59 +0100

    add languages

---
+++ assets/js/app.js
@@ -0,0 +1,18 @@
+$(document).ready(function () {
+
+    // API CodeStats
+    setTimeout(function () {
+        $.getJSON('https://codestats.net/api/users/NHClement', function (data) {
+            $('.spinner').hide();
+            var languages = data.languages;
+            for(index in languages){
+                $('#languages').prepend('  <div class="col-lg-4" style="padding: 10px 0">\n' +
+                    '                                    <p class="title size-5">' + index + '</p>\n' +
+                    '                                    <p class="subtitle size-7 '+ (languages[index].xps < 1000 ? "text-red-500" : languages[index].xps < 5000 ? "text-orange-500" : "text-green-500") +'">' + languages[index].xps + ' lignes tapés.</p>\n' +
+                    '                        </div>');
+            }
+        });
+    }, 1000);
+
+
+});
